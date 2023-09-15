72bb5ab04696cd0e026044d5007c5e37f36d4336
PavloPak <pak.pavlo@gmail.com>
2023-09-15 18:42:44 +0300

    edit push2

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -43,10 +43,10 @@ podTemplate(yaml: '''
             if [ ${PUSH_IMAGE} == "true" ]; then
                  echo "$DOCKER_REGISTRY_PWD" | docker login -u "$DOCKER_REGISTRY_USER" --password-stdin                              
                  echo Login Completed    
-                 docker push ppak4dev/udemy-dmeo-client:${env.BUILD_NUMBER}
-                 docker push ppak4dev/udemy-dmeo-nginx:${env.BUILD_NUMBER}
-                 docker push ppak4dev/udemy-dmeo-server:${env.BUILD_NUMBER}
-                 docker push ppak4dev/udemy-dmeo-worker:${env.BUILD_NUMBER}
+                 docker push ppak4dev/udemy-dmeo-client:"${env.BUILD_NUMBER}"
+                 docker push ppak4dev/udemy-dmeo-nginx:"${env.BUILD_NUMBER}"
+                 docker push ppak4dev/udemy-dmeo-server:"${env.BUILD_NUMBER}"
+                 docker push ppak4dev/udemy-dmeo-worker:"${env.BUILD_NUMBER}"
             fi;   
           '''
          }
