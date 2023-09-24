f6f02374e1ec97c48437c71dbe27aca3fce59cc1
PavloPak <pak.pavlo@gmail.com>
2023-09-24 16:42:27 +0300

    Update 3

--- jenkins/pipeline2.groovy
+++ jenkins/pipeline2.groovy
@@ -1,15 +1,22 @@
 pipeline {
   agent {
     kubernetes {
-		  inheritFrom 'docker-worker-pod'
-	  }
+      inheritFrom 'docker-worker-pod'
+    }
   }
   stages {
     stage('Get project') {
-        steps {
-          echo 'Getting project >> >> >>'
-          git branch: 'main', credentialsId: 'jenkins', url: 'https://github.com/PavloPak/docker-rs'
-        }
-     }
+      steps {
+        echo 'Getting project >> >> >>'
+        git branch: 'main', credentialsId: 'jenkins', url: 'https://github.com/PavloPak/docker-rs'
+      }
+    }
+    stage('Build Docker image') {
+      echo 'Hellooo ! 4  !'
+      sh "docker build -t ppak4dev/udemy-dmeo-client:${env.BUILD_NUMBER} ./client "
+      sh "docker build -t ppak4dev/udemy-dmeo-nginx:${env.BUILD_NUMBER} ./nginx "
+      sh "docker build -t ppak4dev/udemy-dmeo-server:${env.BUILD_NUMBER} ./server "
+      sh "docker build -t ppak4dev/udemy-dmeo-worker:${env.BUILD_NUMBER} ./worker "
+    }
   }
 }
