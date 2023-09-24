4a5f5bb91366e4e4f00090e75d291511a72e0536
PavloPak <pak.pavlo@gmail.com>
2023-09-24 17:34:34 +0300

    Update 7

--- jenkins/pipeline2.groovy
+++ jenkins/pipeline2.groovy
@@ -6,17 +6,18 @@ pipeline {
     stage('Get project') {
       steps {
         echo 'Getting project >> >> >>'
-        git branch: 'main', credentialsId: 'jenkins', url: 'https://github.com/PavloPak/docker-rs'
+        curl zalenium-jenkins.zalenium.svc.cluster.local/wd/hub
+        //git branch: 'main', credentialsId: 'jenkins', url: 'https://github.com/PavloPak/docker-rs'
       }
     }
     stage('Build Docker image') {
       steps {
         container('docker') {
           echo 'Hellooo ! 4  !'
-          sh "docker build -t ppak4dev/udemy-dmeo-client:${env.BUILD_NUMBER} ./client "
-          sh "docker build -t ppak4dev/udemy-dmeo-nginx:${env.BUILD_NUMBER} ./nginx "
-          sh "docker build -t ppak4dev/udemy-dmeo-server:${env.BUILD_NUMBER} ./server "
-          sh "docker build -t ppak4dev/udemy-dmeo-worker:${env.BUILD_NUMBER} ./worker "
+          //sh "docker build -t ppak4dev/udemy-dmeo-client:${env.BUILD_NUMBER} ./client "
+          //sh "docker build -t ppak4dev/udemy-dmeo-nginx:${env.BUILD_NUMBER} ./nginx "
+          //sh "docker build -t ppak4dev/udemy-dmeo-server:${env.BUILD_NUMBER} ./server "
+          //sh "docker build -t ppak4dev/udemy-dmeo-worker:${env.BUILD_NUMBER} ./worker "
         }
       }
     }
