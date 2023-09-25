1e7fe5f6a3f4c7fd6279f73d6cb2d07ea3215e0b
PavloPak <pak.pavlo@gmail.com>
2023-09-25 14:31:09 +0300

    build 1

--- jenkins/pipeline2.groovy
+++ jenkins/pipeline2.groovy
@@ -6,16 +6,16 @@ pipeline {
     stage('Get project') {
       steps {
         echo 'Getting project >> >> >>'
-        sh "curl http://zalenium-jenkins.zalenium.svc.cluster.local/wd/hub"
-        //git branch: 'main', credentialsId: 'jenkins', url: 'https://github.com/PavloPak/docker-rs'
+        git branch: 'main', credentialsId: 'jenkins', url: 'https://github.com/PavloPak/zalando-ts'
+        sh 'echo "$(mvn -v)"'
       }
     }
-    stage('Build Docker image') {
+    stage('Run tests') {
       steps {
         container('docker') {
-          echo 'Hellooo ! 4  !'
-          //sh "docker build -t ppak4dev/udemy-dmeo-client:${env.BUILD_NUMBER} ./client "
-          //sh "docker build -t ppak4dev/udemy-dmeo-nginx:${env.BUILD_NUMBER} ./nginx "
+          echo 'Starting'
+          sh "ls -la"
+          sh "mvn clean test"
           //sh "docker build -t ppak4dev/udemy-dmeo-server:${env.BUILD_NUMBER} ./server "
           //sh "docker build -t ppak4dev/udemy-dmeo-worker:${env.BUILD_NUMBER} ./worker "
         }
