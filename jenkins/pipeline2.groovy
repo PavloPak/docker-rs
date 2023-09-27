8e010968fb7202650d050f07d485728b64c5bce0
PavloPak <pak.pavlo@gmail.com>
2023-09-27 14:28:59 +0300

    Update 1111

--- jenkins/pipeline2.groovy
+++ jenkins/pipeline2.groovy
@@ -23,24 +23,24 @@ pipeline {
           }
          }
        }
-    withCredentials([usernamePassword(credentialsId: 'Docker-Hub-U-P', passwordVariable: 'DOCKER_REGISTRY_PWD', usernameVariable: 'DOCKER_REGISTRY_USER')]) {
-      stage('Push image') {
+    stage('Push image') {
+      withCredentials([usernamePassword(credentialsId: 'Docker-Hub-U-P', passwordVariable: 'DOCKER_REGISTRY_PWD', usernameVariable: 'DOCKER_REGISTRY_USER')]) { 
         steps {
           container('docker') {
-          sh '''
+            sh '''
             set +x
             if [ ${PUSH_IMAGE} == "true" ]; then
-                 echo "$DOCKER_REGISTRY_PWD" | docker login -u "$DOCKER_REGISTRY_USER" --password-stdin                              
-                 echo Login Completed    
-                 docker push ppak4dev/udemy-dmeo-client:"${BUILD_NUMBER}"
-                 docker push ppak4dev/udemy-dmeo-nginx:"${BUILD_NUMBER}"
-                 docker push ppak4dev/udemy-dmeo-server:"${BUILD_NUMBER}"
-                 docker push ppak4dev/udemy-dmeo-worker:"${BUILD_NUMBER}"
+               echo "$DOCKER_REGISTRY_PWD" | docker login -u "$DOCKER_REGISTRY_USER" --password-stdin                              
+               echo Login Completed    
+               docker push ppak4dev/udemy-dmeo-client:"${BUILD_NUMBER}"
+               docker push ppak4dev/udemy-dmeo-nginx:"${BUILD_NUMBER}"
+               docker push ppak4dev/udemy-dmeo-server:"${BUILD_NUMBER}"
+               docker push ppak4dev/udemy-dmeo-worker:"${BUILD_NUMBER}"
             fi;   
           '''
          }
-        }  
+       }  
       }
-     }
+    }
   }
 }
