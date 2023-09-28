074d703725be85573dcc0f7cd9cc706f4f0d9c5e
PavloPak <pak.pavlo@gmail.com>
2023-09-28 13:24:56 +0300

    Update nginx move

--- jenkins/pipeline2.groovy
+++ jenkins/pipeline2.groovy
@@ -15,7 +15,7 @@ pipeline {
           echo 'Hellooo ! 4  !'
           container('docker') {
             sh "docker build -t ppak4dev/udemy-dmeo-client:${env.BUILD_NUMBER} ./client "
-            sh "docker build -t ppak4dev/udemy-dmeo-nginx:${env.BUILD_NUMBER} ./nginx "
+            // sh "docker build -t ppak4dev/udemy-dmeo-nginx:${env.BUILD_NUMBER} ./nginx "
             sh "docker build -t ppak4dev/udemy-dmeo-server:${env.BUILD_NUMBER} ./server "
             sh "docker build -t ppak4dev/udemy-dmeo-worker:${env.BUILD_NUMBER} ./worker "
           }
@@ -31,7 +31,7 @@ pipeline {
                echo "$DOCKER_REGISTRY_PWD" | docker login -u "$DOCKER_REGISTRY_USER" --password-stdin                              
                echo Login Completed    
                docker push ppak4dev/udemy-dmeo-client:"${BUILD_NUMBER}"
-               docker push ppak4dev/udemy-dmeo-nginx:"${BUILD_NUMBER}"
+               // docker push ppak4dev/udemy-dmeo-nginx:"${BUILD_NUMBER}"
                docker push ppak4dev/udemy-dmeo-server:"${BUILD_NUMBER}"
                docker push ppak4dev/udemy-dmeo-worker:"${BUILD_NUMBER}"
             fi;   
